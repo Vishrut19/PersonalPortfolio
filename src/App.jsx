684dfff3fc65584389050dc83ece77daf2130708
Vishrut Agarwalla <vishrutagarwalla19@gmail.com>
2023-06-22 19:47:40 +0530

    Added Navbar, Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Home />
     </>
   );
 };
