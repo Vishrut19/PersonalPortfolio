8d74b8c5543af232ea7f0d6db3f724a27fb8903f
Vishrut Agarwalla <vishrutagarwalla19@gmail.com>
2023-06-22 16:50:35 +0530

    Added First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+};
+
+export default App;
